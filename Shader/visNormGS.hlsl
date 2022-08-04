cadc2095a9f5c8b77f5c97e9aa2e494d49754539
liruntu2333 <liruntu2333@gmail.com>
2022-08-04 15:49:34 +0900

    fix: fix visualized normal color mapping issue

--- Shader/visNormGS.hlsl
+++ Shader/visNormGS.hlsl
@@ -9,9 +9,10 @@ void main(
     GeoOut gout[2];
     gout[0].PosH = mul(float4(gin[0].PosW, 1.0f), gViewProj);
     float3 endPosW = gin[0].PosW + gin[0].NormalW;
+    float3 color = (gin[0].NormalW + 1.0f) * 0.5f;
     gout[1].PosH = mul(float4(endPosW, 1.0f), gViewProj);
-    gout[0].Color = gin[0].NormalW;
-    gout[1].Color = gin[0].NormalW;
+    gout[0].Color = color;
+    gout[1].Color = color;
 
     output.Append(gout[0]);
     output.Append(gout[1]);
